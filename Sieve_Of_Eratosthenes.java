53053a9d94a489b56d61a0922e59f9c55fb206cf
HasibGit <48997902+HasibGit@users.noreply.github.com>
2020-06-02 21:55:44 +0600

    Add files via upload

---
+++ Sieve_Of_Eratosthenes.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Sieve_Of_Eratosthenes {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        boolean[] prime = new boolean[n+1];
+        for(int i = 2;i <= n;i++){
+            prime[i] = true;
+        }
+
+        for(int i = 2;i * i <= n;i++){
+            if(prime[i] == true){
+                for(int j = i * i;j <= n;j += i){
+                    prime[j] = false;
+                }
+            }
+        }
+
+        for(int i = 1;i <= n;i++){
+            if(prime[i]){
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
